0483863a39cd09d0899627acc7d32ced58d7e9b9
mateusfelixdias <felixmateus618@gmail.com>
2022-08-17 16:23:47 -0300

    added component arrow

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,16 @@
+export function Arrow() {
+  return (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="40"
+        fill="none"
+        viewBox="0 0 69 69"
+    >
+        <path
+            fill="#fff"
+            d="M36.647 27.013l-10.59 16.933 3.534 2.209 10.588-16.933 2.841 12.326 4.06-.936-4.438-19.256-19.256 4.438.936 4.06 12.325-2.84z"
+        ></path>
+    </svg>
+  );
+};
