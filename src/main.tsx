762a5946b63c01d0793906459af416073c7521fa
mateusfelixdias <felixmateus618@gmail.com>
2022-11-26 16:01:33 -0300

    refactor: modified style directory name

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { App } from './page/App';
 
-import './main.css';
+import './style/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
