90185614b3bead2af999d96e50f3a8bed72b8ec1
mateusfelixdias <felixmateus618@gmail.com>
2022-11-26 15:46:51 -0300

    feat: added component vector

---
+++ src/components/Vector/index.tsx
@@ -0,0 +1,10 @@
+export function Vector() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='23' height='29' fill='none' viewBox='0 0 23 29'>
+      <path
+        fill='#000'
+        d='M9.442 8.586l.356 20.252 3.333-.094-.357-20.252 7.313 8.867 2.304-3.054L10.966.452.039 14.938l2.41 2.92 6.993-9.272z'
+      ></path>
+    </svg>
+  );
+}
