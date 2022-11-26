2317e3657822b1ded5844ead64ab33815e9769b9
mateusfelixdias <felixmateus618@gmail.com>
2022-11-26 15:46:00 -0300

    refactor: formatted code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       backgroundImage: {
-        blackLine: `url("./src/assets/imgDeFundo.png")`
-      }
+        blackLine: `url("./src/assets/imgDeFundo.png")`,
+      },
+    },
+    screens: {
+      mobile: { max: '700px' },
     },
   },
   plugins: [],
-}
+};
