f4392b45d752a76a2a2d71f3f6aeda5f2b260040
mateusfelixdias <felixmateus618@gmail.com>
2022-11-26 15:40:33 -0300

    feat: artist images, name and photographs

---
+++ src/utils/artists.ts
@@ -0,0 +1,45 @@
+import artistaOne from '../assets/artistaOne.png';
+import artistaTwo from '../assets/artistaTwo.png';
+import artistaThree from '../assets/artistaThree.png';
+import artistaFour from '../assets/artistaFour.png';
+import artistaFive from '../assets/artistaFive.png';
+import artistaSix from '../assets/artistaSix.png';
+
+export const artists = [
+  {
+    artist: artistaOne,
+    name: 'Mateus',
+    fotografias: 80,
+  },
+
+  {
+    artist: artistaThree,
+    name: 'Bruno',
+    fotografias: 70,
+  },
+
+  {
+    artist: artistaSix,
+    name: 'Breno',
+    fotografias: 50,
+  },
+];
+
+export const artistsFeminine = [
+  {
+    artist: artistaTwo,
+    name: 'Joana',
+    fotografias: 60,
+  },
+  {
+    artist: artistaFour,
+    name: 'Ana',
+    fotografias: 40,
+  },
+
+  {
+    artist: artistaFive,
+    name: 'Brena',
+    fotografias: 30,
+  },
+];
