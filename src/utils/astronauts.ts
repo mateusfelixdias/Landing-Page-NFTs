073e80860938fc5026e32d2bb2a99134ea2fe517
mateusfelixdias <felixmateus618@gmail.com>
2022-11-26 15:44:26 -0300

    feat: added all astronaut data

---
+++ src/utils/astronauts.ts
@@ -0,0 +1,30 @@
+import astronautaOne from '../assets/astronautaOne.png';
+import astronautaTwo from '../assets/astronautaTwo.png';
+import astronautaThree from '../assets/astronautaThree.png';
+import astronautaFour from '../assets/astronautaFour.png';
+
+export const dataForTheAstronaut = [
+  {
+    name: 'Bruno',
+    img: astronautaOne,
+    RKT: '1.50 RKT',
+  },
+  {
+    name: 'Mateus',
+    img: astronautaTwo,
+    RKT: '2.0 RKT',
+  },
+];
+
+export const dataForTheAstronautFeminine = [
+  {
+    name: 'Bruna',
+    img: astronautaThree,
+    RKT: '3.75 RKT',
+  },
+  {
+    name: 'Fabiana',
+    img: astronautaFour,
+    RKT: '4.30 RKT',
+  },
+];
